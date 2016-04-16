bf9c955adb6db626a0d0dbaca34cc33f28604a61
skuhlmann <samkuhlmann@gmail.com>
2016-04-16 15:57:49 -0600

    Adds some more basic stlyes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,10 @@
 class HomeController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
 
   def index
+  end
+
+  def dashboard
     @user = current_user
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: { sessions: 'sessions' }
 
+  get '/dashboard', to: 'home#dashboard', as: 'user_root'
+
   root "home#index"
 end
