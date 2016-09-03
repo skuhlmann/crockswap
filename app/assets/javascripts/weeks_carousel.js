bc5608a25f89c967262d53738138448c2e06b38a
skuhlmann <samkuhlmann@gmail.com>
2016-09-02 18:47:56 -0600

    adjusting owl for heroku

--- app/assets/javascripts/weeks_carousel.js
+++ app/assets/javascripts/weeks_carousel.js
@@ -1,10 +1,16 @@
 $(document).ready(function() {
   var owl = $(".owl-carousel");
-  owl.owlCarousel({
-    items : 3,
-  });
 
-  var owlInstance = $(".owl-carousel").data('owlCarousel');
-  var weekIndex = owl.attr('data-current-week-index');
-  owlInstance.jumpTo(weekIndex);
+  if (owl) {
+    // owl.owlCarousel({
+    //   items : 3,
+    // });
+
+    owl.owlCarousel();
+
+
+    var owlInstance = $(".owl-carousel").data('owlCarousel');
+    var weekIndex = owl.attr('data-current-week-index');
+    owlInstance.jumpTo(weekIndex);
+  }
 });
