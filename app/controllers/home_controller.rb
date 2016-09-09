9d841d6d4a09a149b382738c4d0d66a028ecf1ee
skuhlmann <samkuhlmann@gmail.com>
2016-09-09 13:48:58 -0600

    Revert for staging

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,8 @@ class HomeController < ApplicationController
   before_action :authenticate_user!, except: [:index]
 
   def index
-    #temp prod view
-    render 'temp_index', layout: "simple"
+    # #temp prod view
+    # render 'temp_index', layout: "simple"
   end
 
   def swapboard
