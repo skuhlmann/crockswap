6b45f526a4f3806910236cc481fb61acf1345da7
skuhlmann <samkuhlmann@gmail.com>
2016-09-12 14:39:07 -0600

    Production settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,8 +59,8 @@ Rails.application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
-  # config.action_mailer.default_url_options = { host: 'evening-depths-39735.herokuapp.com' }
-  config.action_mailer.default_url_options = { host: 'crockswap.com' }
+
+  config.action_mailer.default_url_options = { host: 'www.crockswap.com' }
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
