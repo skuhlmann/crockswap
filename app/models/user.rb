b1a7396e2ae5964a145834a454ad215417b196cd
skuhlmann <samkuhlmann@gmail.com>
2017-01-11 15:32:53 -0700

    Fixes profile thumbnail issue

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "pizza.svg"
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100#" }, default_url: "pizza.svg"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 
   validates_presence_of :email
