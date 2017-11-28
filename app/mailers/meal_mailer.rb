4c1a4aa26b3680c8a6b3fc6b7992f6bdac2a6034
skuhlmann <samkuhlmann@gmail.com>
2017-11-28 11:35:51 -0700

    Fixes unsub links in emails

--- app/mailers/meal_mailer.rb
+++ app/mailers/meal_mailer.rb
@@ -6,15 +6,17 @@ class MealMailer < ActionMailer::Base
     @meal = meal
     @user = User.find(meal.user_id)
     @group = @meal.week.group
-
+    @member = Member.where(group_id: @group.id, user_id: @user.id)
+    
     mail(to: @user.email,
-         subject: "Your #{@meal.name} was rated on Crockswap!")
+    subject: "Your #{@meal.name} was rated on Crockswap!")
   end
-
+  
   def meal_added_email(user, meal, group)
     @user = user
     @group = group
     @meal = meal
+    @member = Member.where(group_id: @group.id, user_id: @user.id)
 
     mail(to: @user.email,
          subject: "A meal was added to #{@group.name} on Crockswap!")
